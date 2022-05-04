81bd14ea0338ea81cd9b8cdd1be9181c071d5a0d
JulioCesarLB <julio.lifeintech@gmail.com>
2022-05-04 15:42:57 -0300

    Complete

--- src/br/com/fiap/sprint/Sprint.java
+++ src/br/com/fiap/sprint/Sprint.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class Sprint {
 	
 	
-	int eixoX=19, eixoY=12, limiteY=0;
+	int eixoX=1, eixoY=15, limiteY=0, limiteX=0;
 	public static void main(String[] args) {
 		Sprint sprint= new Sprint();
 			
@@ -15,8 +15,17 @@ public class Sprint {
 	}
 	public void run() {
 		
+		boolean vitoria=false;
+		int passos=0;
+		//
+		do {
+		
 		// Escolha da direção
 		String[] comandosVet = {"Frente","Tras","Esquerda","Direita"};
+		boolean verific = false; // boolean para o loop se o comando é possível
+		
+		// loop para verificação do comando
+		do {
 		String comando = (String) JOptionPane.showInputDialog(null,"Suas coordenadas são:"
 				+ "\n  X= "+eixoX+"     Y= "+eixoY+" "
 				+ "\n Escolha o comando", "Comandos",JOptionPane.INFORMATION_MESSAGE, null,comandosVet, comandosVet[0]);
@@ -25,14 +34,10 @@ public class Sprint {
 	
 		// Escolha da quantidade de quadrados
 		int distancia;
-		boolean verific = false; // boolean para o loop se verifica se é um num
-		
-		// loop para verificação de num
-		do {
 			
 			String distanciaStr =JOptionPane.showInputDialog(null,"Suas coordenadas são:"
 					+ "\n  X= "+eixoX+"     Y= "+eixoY+
-					"\n Digite a distância que deseja andar");
+					"\n Digite a distância que deseja andar","Para "+comando,JOptionPane.INFORMATION_MESSAGE);
 			
 			// chama o metodo e verifica se é um num memsmo
 			if(verificaNum(distanciaStr)) {
@@ -45,6 +50,7 @@ public class Sprint {
 				if(comando.equals("Frente")) {
 					if(frente(distancia)) {
 						eixoY+=distancia;
+						passos+=1;
 						verific=true;
 					}else {
 						JOptionPane.showMessageDialog(null, "O distancia escolhida é acima do limite, nessa posição você poderia usar no máximo: "+(limiteY-eixoY)+"\n TENTE NOVAMENTE! ", "Erro", JOptionPane.ERROR_MESSAGE);
@@ -52,29 +58,32 @@ public class Sprint {
 				}else if(comando.equals("Tras")) {
 					if(tras(distancia)) {
 						eixoY-=distancia;
+						passos+=1;
 						verific=true;
 					}else {
-						JOptionPane.showMessageDialog(null, "O distancia escolhida é acima do limite, nessa posição você poderia usar no máximo: "+(0000000)+"\n TENTE NOVAMENTE! ", "Erro", JOptionPane.ERROR_MESSAGE);
+						JOptionPane.showMessageDialog(null, "O distancia escolhida é acima do limite, nessa posição você poderia usar no máximo: "+(eixoY-limiteY)+"\n TENTE NOVAMENTE! ", "Erro", JOptionPane.ERROR_MESSAGE);
 					}
 					
 				}else if (comando.equals("Esquerda")) {
 					if(esquerda(distancia)) {
 						eixoX-=distancia;
+						passos+=1;
 						verific=true;
 					}else {
-						JOptionPane.showMessageDialog(null, "O distancia escolhida é acima do limite, nessa posição você poderia usar no máximo: "+(0000000)+"\n TENTE NOVAMENTE! ", "Erro", JOptionPane.ERROR_MESSAGE);
+						JOptionPane.showMessageDialog(null, "O distancia escolhida é acima do limite, nessa posição você poderia usar no máximo: "+(eixoX-limiteX)+"\n TENTE NOVAMENTE! ", "Erro", JOptionPane.ERROR_MESSAGE);
 					}
 					
 				}else if(comando.equals("Direita")) {
 					if(direita(distancia)) {
 						eixoX+=distancia;
+						passos+=1;
 						verific=true;
 					}else {
-						JOptionPane.showMessageDialog(null, "O distancia escolhida é acima do limite, nessa posição você poderia usar no máximo: "+(0000000)+"\n TENTE NOVAMENTE! ", "Erro", JOptionPane.ERROR_MESSAGE);
+						JOptionPane.showMessageDialog(null, "O distancia escolhida é acima do limite, nessa posição você poderia usar no máximo: "+(limiteX-eixoX)+"\n TENTE NOVAMENTE! ", "Erro", JOptionPane.ERROR_MESSAGE);
 					}
 				}
 				
-				verific=true; // altera a variavel para quebrar o loop
+				 
 			}else {
 				JOptionPane.showMessageDialog(null, "Você digitou algo diferente de um número ou um número negativo em: "+distanciaStr+"\n TENTE NOVAMENTE! ", "Erro", JOptionPane.ERROR_MESSAGE);
 				// não quebra o loop e explica para o user o porque
@@ -82,7 +91,16 @@ public class Sprint {
 			
 		}while(verific==false);
 		
+		//Verifica a vitória
+		if(eixoX>=20 && (eixoY>=2 && eixoY<=4)) {
+			//vitória
+			vitoria=true;
+		}else {
+			//ainda não chegou
+		}
+		}while(vitoria==false);
 		
+		JOptionPane.showMessageDialog(null,"Você ganhou com "+passos+" passos");
 	}
 	public boolean frente(int num) { 
 		//ATRIBUI UM LIMITE MÁXIMO PARA O Y
@@ -118,16 +136,86 @@ public class Sprint {
 	}
 	
 	public boolean tras(int num) {
+		if(eixoX<=17 && eixoX>=1 && (eixoY>=14 && eixoY<=16)) {
+			 limiteY= 14;
+
+		}else if((eixoX<=20 && eixoX>=18) && (eixoY>=11 && eixoY<=16)) {
+			 limiteY=11;
+				
+		}else if(((eixoX<=17 && eixoX>=15) && (eixoY>=8 && eixoY<=13)) || ((eixoX<=14 && eixoX>=12) && (eixoY>=8 && eixoY<=10)) || ((eixoX<=8 && eixoX>=2) && (eixoY>=8 && eixoY<=10))) {
+			 limiteY=8;
+					
+		}else if((eixoX<=11 && eixoX>=9) && (eixoY>=2 && eixoY<=10)) {
+			 limiteY=2;
+			
+		}else if((eixoX<=19 && eixoX>=12) && (eixoY>=2 && eixoY<=4)) {
+			 limiteY=2;
+			
+		}else {
+			System.out.println("ERRO NAS POSIÇÕES");
+		}
 		
-		return false;
+		//VERIFICA SE É POSSÍVEL COM ESSE VALOR SEM QUEBRAR O LIMITE
+		if((eixoY-num)<limiteY) {
+			// não pode
+			return false;
+		}else {
+			return true;
+			//pode
+		}
+		
+		
 	}
 	public boolean esquerda(int num) {
+		if((eixoX<=20 && eixoX>=1) && (eixoY>=14 && eixoY<=16)) {
+			limiteX=1;
+			
+		}else if((eixoX<=20 && eixoX>=15) && (eixoY>=11 && eixoY<=13)) {
+			limiteX=15;
+			
+		}else if((eixoX<=17 && eixoX>=2) && (eixoY>=8 && eixoY<=10)) {
+			limiteX=2;
+		}else if((eixoX<=11 && eixoX>=9) && (eixoY>=5 && eixoY<=7)) {
+			limiteX=9;
+		}else if((eixoX<=19 && eixoX>=9) && (eixoY>=2 && eixoY<=4)) {
+			limiteX=9;
+		}else {
+			System.out.println("ERRO NAS POSIÇÕES");
+		}
 		
-		return false;
+		if((eixoX-num)<limiteX) {
+			// não pode
+			return false;
+		}else {
+			return true;
+			//pode
+		}
 	}
 	public boolean direita(int num) {
+		if((eixoX<=20 && eixoX>=1) && (eixoY>=14 && eixoY<=16)) {
+			limiteX=20;
+			
+		}else if((eixoX<=20 && eixoX>=15) && (eixoY>=11 && eixoY<=13)) {
+			limiteX=20;
+			
+		}else if((eixoX<=17 && eixoX>=2) && (eixoY>=8 && eixoY<=10)) {
+			limiteX=17;
+		}else if((eixoX<=11 && eixoX>=9) && (eixoY>=5 && eixoY<=7)) {
+			limiteX=11;
+		}else if((eixoX<=19 && eixoX>=9) && (eixoY>=2 && eixoY<=4)) {
+			limiteX=21;
+		}else {
+			System.out.println("ERRO NAS POSIÇÕES");
+		}
 		
-		return false;
+		if((eixoX+num)>limiteX) {
+			// não pode
+			return false;
+		}else {
+			return true;
+			//pode
+		}
+		
 	}
 	// metodo que verifica se a string pode virar um int sem erros
 	public boolean verificaNum(String palavra) {
